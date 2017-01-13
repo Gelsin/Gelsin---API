7b4723a1005e43fbe5114b3908a6558092653f6d
alirzayev <alirzayev.07@gmail.com>
2017-01-13 14:41:48 +0400

    |-- Added --|  1. Order, OrderDetails, OrderProducts models added.  2. OrderController added.

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alirzayev
+ * Date: 13/01/2017
+ * Time: 14:39
+ */
+
+namespace App\Http\Controllers;
+
+
+class OrderController extends Controller
+{
+
+}
